2c1a6fcc30ca3237ba01b50a2d9a3b20073075d1
cyanidesayonara <cyanidesayonara@gmail.com>
2019-02-09 13:28:40 +0200

    add notes api

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, jsonify
+app = Flask(__name__, template_folder="./frontend/build")
+
+notes = [
+    {
+        "title": "Title",
+        "content": "Content",
+        "author": "Author",
+    },
+    {
+        "title": "Title",
+        "content": "Content",
+        "author": "Author",
+    },
+]
 
 
 @app.route("/")
-def home():
-    return render_template('index.html')
+def index():
+    return render_template("index.html")
+
+
+@app.route("/api/notes")
+def json_notes():
+    return jsonify(notes)
 
 
 if __name__ == '__main__':
