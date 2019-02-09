a0acb226e3c664bed3a8d5d3cd9c4e0797752c0e
anonOstrich <jesper.kuutti@helsinki.fi>
2019-02-09 12:35:47 +0200

    Input field for name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import InputField from './components/InputField'
+ 
+
+const App = () => {
+
+  const [name, setName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(name)
+    setName('')
+  }
 
 
-const App = () => (<h1>Hello world!</h1>)
+  return (
+    <div>
+    <h1>Anna nimi</h1>
+      <InputField
+      handleSubmit={handleSubmit}
+      handleChange={(event) => {
+        setName(event.target.value)
+      }}
+      value={name}
+      />
+    </div>
+  )
+}
 
 export default App;
