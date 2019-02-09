d8e63ef8315af6cc96f554b12f35488c7ef6f383
Tomi <tomipanula1@gmail.com>
2019-02-09 14:03:37 +0200

    xlsx file parser for names

---
+++ find_info.py
@@ -0,0 +1,20 @@
+from openpyxl import load_workbook
+
+def get_name_info_xlsx(name="Tomi"):
+    book = load_workbook('data/etunimitilasto-2019-01-07-vrk.xlsx')
+    count = 0
+    idxs = [1, 4]
+    for sheet_index in idxs:
+        sheet = book.worksheets[sheet_index]
+        for idx, row in enumerate(sheet.iter_rows(min_row=2, min_col=1, max_row=10000, max_col=1)):
+            if row[0].value == None:
+                break
+            for cell in row:
+                if cell.value.lower() == name.lower():
+                    count = max(count, sheet['B' + str(idx+1)].value)
+    return count
+
+c = get_name_info_xlsx()
+print(c)
+if c < 5000 and c != 0:
+    print("You're special snowflake!")
